fa30142d85e6b7cb949a514b5721b635bdddbe6b
nishaunlawrence <nishaun.lawrence@gmail.com>
2025-02-08 23:24:16 -0500

    Added an image and styled the webpage.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # Routing for your application.
 # Put your routes below this comment
 '''
+
 @app.route('/')
 def home():
     return 'My home page'
@@ -14,8 +15,10 @@ def home():
 def about():
     return render_template('about.html')
 
-
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
